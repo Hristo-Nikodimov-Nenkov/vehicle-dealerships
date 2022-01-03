15a879be11cee814b376b475232a7e27702e3177
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2022-01-03 20:53:28 +0100

    - Added - Base layout and routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import {Route, Routes} from "react-router-dom";
 import './App.css';
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return <>
+      <Header/>
+      <Routes>
+         <Route index element={<Home/>}/>
+         <Route path="*" element={<NotFound/>}/>
+      </Routes>
+      <Footer/>
+   </>
 }
 
 export default App;
